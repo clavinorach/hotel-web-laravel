529330871a86e38c2ee4b591cf4bc46e46e8c838
clavinorach <clavinorach@gmail.com>
2024-06-26 00:38:17 +0700

    fix bug

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -14,10 +14,10 @@
             </div>
             <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 d-flex justify-content-end"> <!-- menambahkan beberapa class untuk mengatur posisi navbar -->
                 <nav class="navigation navbar navbar-expand-md navbar-dark">
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-toggle="" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarsExample04">
+                    <div class="" id="navbarsExample04">
                         <ul class="navbar-nav ml-auto">
                             <li class="nav-item {{ Request::is('/') ? 'active' : '' }}">
                                 <a class="nav-link" href="{{url('/')}}" style="padding-top: 9px;">
