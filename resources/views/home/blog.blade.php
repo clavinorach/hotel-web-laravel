17fcd38e5a7624a20c10f72c83fe82ae73781a3b
Babydevil14 <amandapuspitasari066@gmail.com>
2024-06-26 00:35:49 +0700

    ad xixixi

--- resources/views/home/blog.blade.php
+++ resources/views/home/blog.blade.php
@@ -4,7 +4,7 @@
                <div class="col-md-12">
                   <div class="titlepage">
                      <h2>Blog</h2>
-                     <p>Lorem Ipsum available, but the majority have suffered </p>
+                     <p>Detail lebih tentang produk kami</p>
                   </div>
                </div>
             </div>
@@ -17,7 +17,7 @@
                      <div class="blog_room">
                         <h3>Regular Room</h3>
                         <span>The standard chunk </span>
-                        <p>If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generatorsIf you are   </p>
+                        <p>Kamar Reguler menawarkan kenyamanan dasar dengan fasilitas yang memadai untuk memastikan masa menginap yang menyenangkan. Kamar ini dilengkapi dengan tempat tidur yang nyaman, AC, TV layar datar, dan kamar mandi pribadi dengan perlengkapan mandi gratis. Ideal untuk pelancong dengan anggaran terbatas yang menginginkan tempat istirahat yang bersih dan nyaman. </p>
                      </div>
                   </div>
                </div>
@@ -29,7 +29,7 @@
                      <div class="blog_room">
                         <h3>Premium Room</h3>
                         <span>The premium room </span>
-                        <p>If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generatorsIf you are   </p>
+                        <p>Kamar Premium memberikan pengalaman yang lebih baik dengan ruang yang lebih luas dan fasilitas tambahan. Selain fasilitas dasar yang tersedia di Kamar Reguler, Kamar Premium dilengkapi dengan minibar, pembuat kopi/teh, dan area duduk yang lebih nyaman. Interior yang elegan dan layanan kamar yang lebih baik menjadikan Kamar Premium pilihan tepat bagi tamu yang menginginkan kenyamanan ekstra. </p>
                      </div>
                   </div>
                </div>
@@ -41,7 +41,7 @@
                      <div class="blog_room">
                         <h3>Deluxe Room</h3>
                         <span>The deluxe room </span>
-                        <p>If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generatorsIf you are   </p>
+                        <p>Kamar Deluxe adalah pilihan terbaik kami yang menawarkan kemewahan dan kenyamanan tertinggi.Fasilitasnya meliputi tempat tidur king size yang sangat nyaman, AC canggih, TV layar lebar dengan saluran internasional, minibar lengkap, mesin kopi espresso, dan area kerja yang luas. Kamar mandi dilengkapi dengan bathtub, shower terpisah, dan perlengkapan mandi eksklusif. </p>
                      </div>
                   </div>
                </div>
